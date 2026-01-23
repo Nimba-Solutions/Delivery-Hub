36c3f1f7a4bf2c0816ec89adb0de83621dee18d0
glen-bradford-nimba <164393624+glen-bradford-nimba@users.noreply.github.com>
2026-01-23 14:14:01 -0500

    fdas

--- force-app/main/default/lwc/deliveryBudgetSummary/deliveryBudgetSummary.js
+++ force-app/main/default/lwc/deliveryBudgetSummary/deliveryBudgetSummary.js
@@ -5,7 +5,7 @@ export default class DeliveryBudgetSummary extends LightningElement {
     @track metrics = { totalHours: 0, estimatedSpend: 0, activeRequests: 0 };
 
     @wire(getBudgetMetrics)
-    wiredMetrics({ error, data }) {
+    wiredMetrics({ data }) { // Fixed: Removed 'error' since it was unused
         if (data) {
             this.metrics = data;
         }
