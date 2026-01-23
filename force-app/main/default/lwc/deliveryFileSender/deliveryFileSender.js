1bb280301e59a72b517ec4ab30275b331e189a5d
glen-bradford-nimba <164393624+glen-bradford-nimba@users.noreply.github.com>
2026-01-23 13:08:08 -0500

    fdas

--- force-app/main/default/lwc/deliveryFileSender/deliveryFileSender.js
+++ force-app/main/default/lwc/deliveryFileSender/deliveryFileSender.js
@@ -14,7 +14,7 @@ export default class DeliveryFileSender extends LightningElement {
         relatedListId: 'ContentDocumentLinks',
         fields: ['ContentDocumentLink.ContentDocumentId', 'ContentDocumentLink.ContentDocument.Title']
     })
-    wiredFiles({ error, data }) {
+    wiredFiles({ data }) { // FIX: Removed 'error' parameter since it wasn't used
         if (data) {
             this.files = data.records.map(record => ({
                 Id: record.fields.ContentDocumentId.value,
@@ -24,13 +24,11 @@ export default class DeliveryFileSender extends LightningElement {
     }
 
     handleUploadFinished() {
-        // Refresh the list (Automatic via wire usually, or refreshApex)
         this.dispatchEvent(new ShowToastEvent({
             title: 'File Attached',
             message: 'Now click "Send to Dev" to transfer it.',
             variant: 'success'
         }));
-        // In a real app, use refreshApex here
     }
 
     handleSend(event) {
